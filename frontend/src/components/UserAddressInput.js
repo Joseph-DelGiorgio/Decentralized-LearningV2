d4d5f4ce6ed8d7740657e26f0959c5570ba65791
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2024-05-29 14:56:31 -0700

    Update UserAddressInput.js

--- frontend/src/components/UserAddressInput.js
+++ frontend/src/components/UserAddressInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const UserAddressInput = ({ userAddress, onAddressChange, onBalanceCheck }) => (
-  <div>
+  <div className="user-address-input">
     <label htmlFor="userAddress">User Address:</label>
     <input type="text" id="userAddress" value={userAddress} onChange={onAddressChange} />
     <button onClick={onBalanceCheck}>Check Balance</button>
